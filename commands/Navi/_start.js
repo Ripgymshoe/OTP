05376b88b1f24c3797e07cfd32e74b3267351d2f
Ripgymshoe <119703242+Ripgymshoe@users.noreply.github.com>
2022-12-05 07:58:29 -0500

    Update _start.js

--- commands/Navi/_start.js
+++ commands/Navi/_start.js
@@ -6,22 +6,13 @@
   folder: Navi
 
   <<ANSWER
-🥾GymshoeOTP Subscription Prices 🎊
-
-1Day | $20
-4Day | $60
-1week | 100$
-1Month | $300$
-2Month | $650
-4Month | $1,200
-6Month | $2,000
-Lifetime | $5,000
+What Can I help you with today?
 
 Our channel - https://t.me/GymshoeOTP
 
 Admin - @GymshoeAdmin 
   ANSWER
-  keyboard: Purchase, /Help
+  keyboard: Start Suscription, Bank, Cash App, /Start
   aliases: 
 CMD*/
 
@@ -41,30 +32,4 @@ Bot.sendInlineKeyboard(
   buttons,
   "Please select membership. After purchase, contact Admin for authentication Key "
   )
-// Install the Java helper library from twilio.com/docs/libraries/java
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import com.twilio.Twilio;
-import com.twilio.rest.api.v2010.account.Call;
-import com.twilio.type.PhoneNumber;
-
-public class MakePhoneCall {
-    // Get your Account SID and Auth Token from https://twilio.com/console
-    // To set up environment variables, see http://twil.io/secure
-    public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
-    public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
-
-    public static void main(String[] args) throws URISyntaxException {
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-
-        String from = "+15017122661";
-        String to = "+14155551212";
-
-        Call call = Call.creator(new PhoneNumber(to), new PhoneNumber(from),
-                new URI("http://demo.twilio.com/docs/voice.xml")).create();
-
-        System.out.println(call.getSid());
-    }
-}
 
