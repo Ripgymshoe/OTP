4498b4bab3a8e49eb37fdffd5e6ab6b6fe09f0d2
Bots.Business <git.sync@bots.business>
2022-12-05 09:47:01 +0000

    Bots.Business export for bot id: 775354

--- commands/MESSAGE ME/_start.js
+++ commands/MESSAGE ME/_start.js
@@ -3,7 +3,7 @@
   help: 
   need_reply: true
   auto_retry_time: 
-  folder: Navi
+  folder: MESSAGE ME
 
   <<ANSWER
 🥾GymshoeOTP Subscription Prices 🎊
@@ -21,7 +21,10 @@ Our channel - https://t.me/GymshoeOTP
 
 Admin - @GymshoeAdmin 
   ANSWER
-  keyboard: Purchase, /Help
+
+  <<KEYBOARD
+
+  KEYBOARD
   aliases: 
 CMD*/
 
@@ -41,30 +44,31 @@ Bot.sendInlineKeyboard(
   buttons,
   "Please select membership. After purchase, contact Admin for authentication Key "
   )
-// Install the Java helper library from twilio.com/docs/libraries/java
-import java.net.URI;
-import java.net.URISyntaxException;
+<Response>
+  <Message>
+  How Can I Further Assist You
+  </Message>
+</Response>
 
-import com.twilio.Twilio;
-import com.twilio.rest.api.v2010.account.Call;
-import com.twilio.type.PhoneNumber;
+var buttons = [
+  { title: "Cash App 1Day $20", url: "https://cash.app/launch/bitcoin/$bayseanw/U3rFTjO7uX" },
+  { title: "Cash App 4Day $60", url: "https://cash.app/launch/bitcoin/$bayseanw/U3rFTjO7uX" },
+  { title: "Cash App 1week $100", url: "https://cash.app/launch/bitcoin/$bayseanw/U3rFTjO7uX" },     
+  { title: "Cash App 1Month $300", url: "https://cash.app/launch/bitcoin/$bayseanw/U3rFTjO7uX" },
+  { title: "Cash App 2month $650", url: "https://cash.app/launch/bitcoin/$bayseanw/U3rFTjO7uX" },
+  { title: "Cash App 4month $1,200", url: "https://cash.app/launch/bitcoin/$bayseanw/U3rFTjO7uX" },  
+  { title: "Cash App 6months $2,000", url: "https://cash.app/launch/bitcoin/$bayseanw/U3rFTjO7uX" },  
+  { title: "Cash App Lifetime $,5000", url: "https://cash.app/launch/bitcoin/$bayseanw/U3rFTjO7uX" },
 
-public class MakePhoneCall {
-    // Get your Account SID and Auth Token from https://twilio.com/console
-    // To set up environment variables, see http://twil.io/secure
-    public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
-    public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
+]
 
-    public static void main(String[] args) throws URISyntaxException {
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-
-        String from = "+15017122661";
-        String to = "+14155551212";
-
-        Call call = Call.creator(new PhoneNumber(to), new PhoneNumber(from),
-                new URI("http://demo.twilio.com/docs/voice.xml")).create();
-
-        System.out.println(call.getSid());
-    }
-}
+Bot.sendInlineKeyboard(
+  buttons,
+  "Please select membership. After purchase, contact Admin for authentication Key "
+  )
+<Response>
+  <Message>
+  How Can I Further Assist You
+  </Message>
+</Response>
 
